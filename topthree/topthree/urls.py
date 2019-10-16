39a6625e6d598a0d012597e209c8c29ddd44422c
ayushagarwal28 <simplyayush28@gmail.com>
2019-10-16 18:42:16 +0530

    Minor updates in the default boiler plate.

--- topthree/topthree/urls.py
+++ topthree/topthree/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('time/', views.curr_date_time),
     path('home/', views.home),
+    path('polls/', include('polls.urls')),
 ]
