4c954c3d8893affbc8a88febcf8ea0ff8e0fe9ba
ayushagarwal28 <simplyayush28@gmail.com>
2019-10-17 15:17:01 +0530

    Included Bootstrap CDN

--- topthree/topthree/views.py
+++ topthree/topthree/views.py
@@ -4,9 +4,8 @@ import datetime
 
 def curr_date_time(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html" % now
+    html = "<html><body><h1>It is now %s.</h1></body></html" % now
     return HttpResponse(html)
 
 def home(request):
-    context = {'insert_me' : 'This is my dynamic data'}
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
